06de5484893db0e208803091fdf5a4400d66c9d4
jossanchezal <jossanchezal@unal.edu.co>
2024-11-15 21:50:10 -0500

    14

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -4,49 +4,35 @@ public class Control{
     private TV tv;
 
     public void turnOn(){
-        if(tv.getEstado() == true){
         tv.turnOn();
-        }
     }
     public void turnOff(){
-        if(tv.getEstado() == true){
         tv.turnOff();
-        }
     }
     public void canalUp(){
-        if(tv.getEstado() == true){
         tv.canalUp();
-        }
     }
     public void canalDown(){
-        if(tv.getEstado() == true){
         tv.canalDown();
-        }
     }
     public void volumenUp(){
-        if(tv.getEstado() == true){
         tv.volumenUp();
-        }
     }
     public void volumenDown(){
-        if(tv.getEstado() == true){
         tv.volumenDown();
-        }
     }
     public void setCanal(int canal){
-        if(tv.getEstado() == true){
+
         tv.setCanal(canal);
-        }
     }
     public void setVolumen(int volumen){
-        if(tv.getEstado() == true){
         tv.setVolumen(volumen);
-        }
+    
     }
     public void enlazar(TV tv){
-        if(tv.getEstado() == true){
         this.tv= tv;
-        }
+        tv.setControl(this);
+        
     }
     public TV getTv(){
         return tv;
